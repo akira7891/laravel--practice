7f8ac711edd7a96886923c9c7ab017ec5b25dd51
akira <akiraakira462@gmail.com>
2022-05-27 21:14:35 +0900

    ブレードテンプレートにコントローラーで定義した変数を出力

--- app/Http/Controllers/Test/IndexController.php
+++ app/Http/Controllers/Test/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Controller
 {
   
    public function __invoke(Request $request, Response $response) {
-       return view('php.index');
+       return view('php.index',["res"=>$response]);
    }
 
 }
--- resources/views/php/index.blade.php
+++ resources/views/php/index.blade.php
@@ -3,12 +3,12 @@
    <title>Hello/Index</title>
    <style>
    body {font-size:16pt; color:#999; }
-   h1 { font-size:100pt; text-align:right; color:#f6f6f6;
+   h1 { font-size:100pt; text-align:left; color:#f6f6f6;
        margin:-50px 0px -100px 0px; }
    </style>
 </head>
 <body>
    <h1>Index</h1>
-   <p>This is a sample page with php-template.</p>
+   <p>{{$res}}</p>
 </body>
 </html>
