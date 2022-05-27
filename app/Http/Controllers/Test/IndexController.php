b75c5e2c28b157495108ba64613ae819a9e5b86d
akira <akiraakira462@gmail.com>
2022-05-27 21:11:18 +0900

    ブレードテンプレートを出力

--- app/Http/Controllers/Test/IndexController.php
+++ app/Http/Controllers/Test/IndexController.php
@@ -10,17 +10,7 @@ class IndexController extends Controller
 {
   
    public function __invoke(Request $request, Response $response) {
-
-$html = <<<EOF
-
-  <h3>リクエスト</h3>
-  <pre>{$request}</pre>
-  <h3>レスポンス</h3>
-  <pre>{$response}</pre>
-
-EOF;
-     
-       return $html;
+       return view('php.index');
    }
 
 }
